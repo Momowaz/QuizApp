4db6ba0e5a8a60159bf3d672191e2643d6f4aa6d
Mitali <mitali.kawde11@gmail.com>
2023-05-31 12:11:12 -0700

    added post routes for login, logout, register

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,60 @@
  */
 
 const express = require('express');
+const bcrypt = require("bcrypt");
+// const database = require("../db/database");
+
 const router  = express.Router();
 
 router.get('/', (req, res) => {
   res.render('users');
-}); 
+});
+
+
+// Log a user in
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // database.getUserWithEmail(email).then((user) => {
+  //   if (!user) {
+  //     return res.send({ error: "no user with that id" });
+  //   }
+
+  //   if (!bcrypt.compareSync(password, user.password)) {
+  //     return res.send({ error: "error" });
+  //   }
+
+    // set cookie
+    req.session.userId = 123; //user.id;
+    res.redirect('/');
+  // });
+});
+
+
+// Create a new user
+router.post("/register", (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+  // database
+  //   .addUser(user)
+  //   .then((user) => {
+  //     if (!user) {
+  //       return res.send({ error: "error" });
+  //     }
+
+      req.session.userId = 123; //user.id;
+      res.redirect('/');
+    // })
+    // .catch((e) => res.send(e));
+});
+
+// Log a user out
+router.post("/logout", (req, res) => {
+  req.session.userId = null;
+  res.redirect("/login");
+});
+
+
 
 module.exports = router;
